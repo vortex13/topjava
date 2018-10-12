14eb38ca686d33184283e00e2fafb5645eed7e76
Терещенко Александр <atereshchenko@elar.ru>
2018-10-12 17:51:07 +0300

    Добавляет обработку в сервлете

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -16,8 +16,9 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 
 public class MealsUtil {
-    public static void main(String[] args) {
-        List<Meal> meals = Arrays.asList(
+    private static final List<Meal> meals;
+    static {
+        meals = Arrays.asList(
                 new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
                 new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
                 new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
@@ -25,6 +26,9 @@ public class MealsUtil {
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
+    }
+
+    public static void main(String[] args) {
         List<MealWithExceed> mealsWithExceeded = getFilteredWithExceeded(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsWithExceeded.forEach(System.out::println);
 
@@ -107,5 +111,18 @@ public class MealsUtil {
 
     public static MealWithExceed createWithExceed(Meal meal, boolean exceeded) {
         return new MealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), exceeded);
+    }
+
+    public static List<MealWithExceed> getWithExceeded(List<Meal> meals, int caloriesPerDay) {
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect( Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)) );
+
+        return meals.stream()
+                .map(meal -> createWithExceed(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
+                .collect(toList());
+    }
+
+    public static List<Meal> getMeals() {
+        return meals;
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@WebServlet("/meals")
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int exceed = Integer.parseInt(request.getParameter("exceed"));
+        List<MealWithExceed> meals = MealsUtil.getWithExceeded(MealsUtil.getMeals(), exceed);
+        request.setAttribute("deals", meals);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        //TODO make table in jsp
+    }
+}
