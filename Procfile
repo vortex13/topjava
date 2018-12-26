1889df0ee0ac37573ee4fb5c083f4eb268102566
Терещенко Александр <atereshchenko@elar.ru>
2018-12-26 11:17:22 +0300

    11.11

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -Dspring.profiles.active="datajpa,heroku" -DTOPJAVA_ROOT="." -classpath target/dependency webapp.runner.launch.Main --port $PORT target/*.war
---
+++ hr.bat
@@ -0,0 +1,3 @@
+call mvn -B -s settings.xml -DskipTests=true clean package
+rem call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
+call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -classpath target/dependency webapp.runner.launch.Main target/*.war
